b9d6a571817c31fe9d683a0cdd1c9d722096a2a1
davnube <dav.nube@gmail.com>
2024-06-13 12:59:51 -0600

    create proyect

---
+++ src/test/java/com/data/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
